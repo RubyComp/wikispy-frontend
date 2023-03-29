0ebf8a07d3edc66736af4a7a05b7a9c6189da6a9
RubyComp <alex61225@gmail.com>
2023-03-29 14:39:22 +0300

    add footer

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,6 +3,7 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
 import Header from '../parts/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from '../parts/Footer'
 
 const Layout = () => {
 	return (
@@ -14,6 +15,9 @@ const Layout = () => {
 				<Col>
 					<Outlet />
 				</Col>
+			</Row>
+			<Row>
+				<Footer />
 			</Row>
 		</Container>
 	)
---
+++ src/parts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className="d-flex justify-content-end">
+			<div><small>GitHub: <a href="https://github.com/RubyComp/wikispy-frontend" target="_blank" rel="noreferrer">Fronted</a>, <a href="https://github.com/RubyComp/wikispy-backend" target="_blank" rel="noreferrer">Backend</a></small></div>
+		</footer>
+	)
+}
+
+export default Footer
